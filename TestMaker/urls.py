283c27497a53cec74746f5615af77cc22e178a5b
albinpaul <albinpaul.ks@gmail.com>
2017-02-15 22:39:50 +0530

    very basic setup, ask any doubts about the directory structure then ill explain

--- TestMaker/urls.py
+++ TestMaker/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    #this is the url for adding the  urls in the accounts app,which are there in urls.py in accounts app
+    url(r'',include('accounts.urls')),
+    #likewise in compiler
+
 ]
